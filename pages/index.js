ab352e4dcae3b2935331ff7d460dc46521e7a266
Pezzutti <jp.pezzutti@gmail.com>
2021-07-19 22:28:33 -0300

    5ta aula

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 import {AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons';
@@ -33,8 +35,8 @@ function ProfileRelationsBox(props){
   )
 }
 
-export default function Home() {
-  const githubUser = "pezzutti";
+export default function Home(props) {
+  const githubUser = props.githubUser;
   const favoritePeople = ['juunegreiros','omariosouto','peas','rafaballerini','marcobrunodev','felipefialho'];
 
   const [communities, setCommunities] = React.useState([]);
@@ -178,4 +180,32 @@ export default function Home() {
      </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN;
+
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then((resposta) => resposta.json())
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    },
+  }
 }
